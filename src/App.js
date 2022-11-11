bbe767621e09cc4395b355ffbbac55c5d555bf12
Kévin Gentilini <kevingentilini@macbook-pro.home>
2022-11-11 21:37:17 +0100

    cart add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,7 +23,29 @@ function App() {
     };
     fetchData();
   }, []);
-
+  const handleCart = (meal) => {
+    const newCart = [...cart];
+    const newMeal = { ...meal, quantity: 1 };
+    const mealExists = newCart.find((element) => element.id === meal.id);
+    if (mealExists) {
+      mealExists.quantity++;
+    } else {
+      newCart.push(newMeal);
+    }
+    setCart(newCart);
+  };
+  const removeToCart = (meal) => {
+    const newCart = [...cart];
+    const mealInTab = newCart.find((element) => element.id === meal.id);
+    if (mealInTab.quantity === 1) {
+      const index = newCart.indexOf(mealInTab);
+      newCart.splice(index, 1);
+    } else {
+      mealInTab.quantity--;
+    }
+    setCart(newCart);
+  };
+  let total = 0;
   return isLoading ? (
     <span>En cours de chargement</span>
   ) : (
@@ -44,6 +67,7 @@ function App() {
             return (
               category.meals.length > 0 && (
                 <Category
+                  handleCart={handleCart}
                   className="container"
                   category={category}
                   key={index}
@@ -51,6 +75,33 @@ function App() {
               )
             );
           })}
+        </div>
+        <div className="main-right">
+          {cart.map((meal, index) => {
+            total = total + meal.quantity * meal.price;
+            return (
+              <div key={meal.id}>
+                <button
+                  onClick={() => {
+                    handleCart(meal);
+                  }}
+                >
+                  +
+                </button>
+                <span>{meal.quantity}</span>
+                <button
+                  onClick={() => {
+                    removeToCart(meal);
+                  }}
+                >
+                  -
+                </button>
+                <span>{meal.title}</span>
+                <span>{(meal.quantity * meal.price).toFixed(2)} €</span>
+              </div>
+            );
+          })}
+          <div>Total: {total.toFixed(2)}</div>
         </div>
       </main>
     </div>
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,17 @@
-const Category = ({ category }) => {
+const Category = ({ category, handleCart }) => {
   return (
     <div>
       <h3>{category.name}</h3>
       <div className="meals-container">
         {category.meals.map((meal, num) => {
           return (
-            <article key={meal.id} className="meal-container">
+            <article
+              onClick={() => {
+                handleCart(meal);
+              }}
+              key={meal.id}
+              className="meal-container"
+            >
               <div>
                 <h4>{meal.title}</h4>
                 <p>{meal.description}</p>
